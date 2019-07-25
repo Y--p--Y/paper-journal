ed9c061ee3a168d48f406d3940b76d7a5a8346a0
Rebecca Lu <lbjrebecca@gmail.com>
2019-07-25 21:41:34 +0800

    fix post style

--- components/markdown/index.js
+++ components/markdown/index.js
@@ -5,6 +5,16 @@ const a = props => (
   />
 )
 
+const blockquote = props => (
+  <section className="flex p-4 md:pr-6 my-2 mx-auto bg-purple-100">
+    <div className="hidden sm:block text-2xl mr-3">💬</div>
+    <blockquote
+      className="flex-grow text-gray-600"
+      {...props}
+    />
+  </section>
+)
 export default {
-  a
+  a,
+  blockquote
 }
